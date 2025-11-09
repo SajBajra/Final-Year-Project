579add0a56ed85b82f96cd6c9aeaee9cc3f45cf0
SajBajra <sajesh.bajracharya.01@gmail.com>
2025-11-09 19:05:12 +0545

    feat: Add admin routes to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,22 +4,54 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import Features from './pages/Features'
 import About from './pages/About'
+import AdminLayout from './components/AdminLayout'
+import AdminDashboard from './pages/admin/AdminDashboard'
+import AdminOCRHistory from './pages/admin/AdminOCRHistory'
+import AdminSettings from './pages/admin/AdminSettings'
+import { ROUTES } from './config/constants'
 import './index.css'
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gradient-to-br from-blue-50 via-indigo-50 via-purple-50 to-pink-50 flex flex-col">
-        <Header />
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/features" element={<Features />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
-        <Footer />
-      </div>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={
+          <div className="min-h-screen bg-gray-50 flex flex-col">
+            <Header />
+            <div className="flex-grow">
+              <Home />
+            </div>
+            <Footer />
+          </div>
+        } />
+        <Route path="/features" element={
+          <div className="min-h-screen bg-gray-50 flex flex-col">
+            <Header />
+            <div className="flex-grow">
+              <Features />
+            </div>
+            <Footer />
+          </div>
+        } />
+        <Route path="/about" element={
+          <div className="min-h-screen bg-gray-50 flex flex-col">
+            <Header />
+            <div className="flex-grow">
+              <About />
+            </div>
+            <Footer />
+          </div>
+        } />
+        
+        {/* Admin Routes */}
+        <Route path={ROUTES.ADMIN} element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="ocr-history" element={<AdminOCRHistory />} />
+          <Route path="settings" element={<AdminSettings />} />
+        </Route>
+      </Routes>
     </Router>
   )
 }
