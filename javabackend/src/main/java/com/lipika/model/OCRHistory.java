a9676f68b4e272b6c359e6c4f3e5f064da0b3e9a
SajBajra <sajesh.bajracharya.01@gmail.com>
2025-11-16 06:23:23 +0545

    fix: Add JsonIgnore to User relationship to prevent lazy loading serialization issues

--- javabackend/src/main/java/com/lipika/model/OCRHistory.java
+++ javabackend/src/main/java/com/lipika/model/OCRHistory.java
@@ -1,5 +1,6 @@
 package com.lipika.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +19,18 @@ public class OCRHistory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @Column(name = "user_id")
+    private Long userId;
+    
+    @Column(name = "is_registered", nullable = false)
+    private Boolean isRegistered = false;
+    
+    @Column(name = "ip_address", length = 45)
+    private String ipAddress;
+    
+    @Column(name = "cookie_id", length = 255)
+    private String cookieId;
+    
     @Column(name = "image_filename", length = 500)
     private String imageFilename;
     
@@ -35,6 +48,11 @@ public class OCRHistory {
     
     @Column(name = "language", length = 50)
     private String language;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    @JsonIgnore // Prevent lazy loading issues during JSON serialization
+    private User user;
     
     @PrePersist
     protected void onCreate() {
